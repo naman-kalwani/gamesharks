6f798ef81c976bcc68b77826cf322583b6467d19
Naman <kalwani.naman1406@gmail.com>
2022-12-06 23:51:53 +0530

    dropdown added

---
+++ js/script.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const header = document.querySelector(".header");
+const sectionTrending = document.querySelector("#section-trending");
+const cardOverlay = document.querySelectorAll(".card-overlay");
+const Cards = document.querySelectorAll(".card");
+
+// modal window ===========================================
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+// ======================================================
+
+// side bar
+
+const sidebarNav = document.querySelector(".sidebar-nav");
+const sidebarLinks = document.querySelectorAll(".sidebar-nav-link");
+
+sidebarNav.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("sidebar-nav-link")) {
+    // scroll
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+
+    // left-border
+    sidebarLinks.forEach((link) => link.classList.remove("active"));
+    e.target.classList.add("active");
+  }
+});
+
+//  fade animation - sidebar ============================
+/*
+const handleHover = function (e) {
+  if (e.target.classList.contains("sidebar-nav-link")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".sidebar-nav")
+      .querySelectorAll(".sidebar-nav-link");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+sidebarNav.addEventListener("mouseover", handleHover.bind(0.4));
+sidebarNav.addEventListener("mouseout", handleHover.bind(1));
+*/
+// ========================================================
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
---
+++ js/tournament.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+// modal window ===========================================>
+/*
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+*/
+// filter btn drop down
+
+const dropdown = document.querySelectorAll(".dropdown");
+const div = dropdown.forEach((dropdown) => {
+  const caret = dropdown.querySelector(".caret");
+  const select = dropdown.querySelector(".select");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll(".menu li ");
+  const selected = dropdown.querySelector(".selected");
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+  options.forEach((option) => {
+    console.log(option);
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+    });
+  });
+});
